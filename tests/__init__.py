47e4a73b6e0702e1a7c2aae44ec62d6d8fe2d861
Eric Wald <eswald@powerspeak.com>
2010-06-25 14:20:23 -0600

    Stuffing the test path manipulation into functions, to declutter the testing namespace.

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,25 +9,29 @@ try:
 except ImportError:
     import unittest
 
-import sys
-from os.path import abspath, dirname, join
-
-root_path = dirname(dirname(abspath((__file__))))
-google_path = join(dirname(root_path), "google_appengine")
-sys.path.insert(0, join(google_path, "lib", "yaml", "lib"))
-sys.path.insert(0, join(google_path, "lib", "antlr3"))
-sys.path.insert(0, join(google_path, "lib", "ipaddr"))
-sys.path.insert(0, join(google_path, "lib", "django"))
-sys.path.insert(0, join(google_path, "lib", "webob"))
-sys.path.insert(0, google_path)
-
-from google.appengine.tools import dev_appserver
-from google.appengine.tools.dev_appserver_main import *
-
-option_dict = DEFAULT_ARGS.copy()
-option_dict[ARG_CLEAR_DATASTORE] = True
+def fixpaths():
+    import sys
+    from os.path import abspath, dirname, join
+    
+    root_path = dirname(dirname(abspath((__file__))))
+    google_path = join(dirname(root_path), "google_appengine")
+    sys.path.insert(0, join(google_path, "lib", "yaml", "lib"))
+    sys.path.insert(0, join(google_path, "lib", "antlr3"))
+    sys.path.insert(0, join(google_path, "lib", "ipaddr"))
+    sys.path.insert(0, join(google_path, "lib", "django"))
+    sys.path.insert(0, join(google_path, "lib", "webob"))
+    sys.path.insert(0, google_path)
+    
+    return root_path
+root_path = fixpaths()
 
 def setup():
+    from google.appengine.tools import dev_appserver
+    from google.appengine.tools.dev_appserver_main import *
+    
+    option_dict = DEFAULT_ARGS.copy()
+    option_dict[ARG_CLEAR_DATASTORE] = True
+    
     logging.basicConfig(level=option_dict[ARG_LOG_LEVEL],
         format="%(levelname)-8s %(asctime)s %(filename)s] %(message)s")
     config, matcher = dev_appserver.LoadAppConfig(root_path, {})
