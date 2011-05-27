861d75ff3b12bae57173ada0ce9f38b5ee279166
Eric Wald <eswald@gmail.com>
2011-05-26 21:51:36 -0600

    Now appends new rank locations to the end, as necessary.

--- static/voting.js
+++ static/voting.js
@@ -2,7 +2,13 @@ var enable_voting = function() {
 	$(".ranking").sortable({
 		connectWith: ".ranking",
 		receive: function(event, ui) {
-			ui.item.find('input').val($(event.target).find('input').val());
+			var target = $(event.target);
+			var newval = target.find('.rankvalue').val();
+			ui.item.find('input').val(newval);
+			if (!target.next().is(".ranking")) {
+				target.after('<div class="ranking"><input type="number" class="rankvalue" value="{val}"></div>'.replace("{val}", Number(newval) + 1));
+				enable_voting();
+			}
 		}
 	}).disableSelection();
 };
