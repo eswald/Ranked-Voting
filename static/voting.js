33fbdfb8b825a59612db52ba619d93d5ca55245e
Eric Wald <eswald@gmail.com>
2011-05-26 22:35:52 -0600

    Now updates all values when adding new ranks.

--- static/voting.js
+++ static/voting.js
@@ -1,34 +1,39 @@
 var enable_voting = function() {
-	var new_rank = '<div class="ranking"><input type="number" class="rankvalue" value="{val}"></div>';
+	var new_rank = '<div class="ranking"><input type="number" class="rankvalue" value="0"></div>';
 	
 	$(".ranking").sortable({
 		connectWith: ".ranking",
 		receive: function(event, ui) {
 			var target = $(event.target);
 			var refilled = false;
-			var newval = target.find('.rankvalue').val();
-			ui.item.find('input').val(newval);
 			
 			var next = target.next();
 			if (!next.is(".ranking")) {
-				target.after(new_rank.replace("{val}", Number(newval) + 1));
+				target.after(new_rank);
 				refilled = true;
 			} else if (next.has(".candidate").length) {
-				target.after(new_rank.replace("{val}", Number(newval) + 1));
+				target.after(new_rank);
 				refilled = true;
 			}
 			
 			var prev = target.prev();
 			if (!prev.is(".ranking")) {
-				target.before(new_rank.replace("{val}", Number(newval) - 1));
+				target.before(new_rank);
 				refilled = true;
 			} else if (prev.has(".candidate").length) {
-				target.before(new_rank.replace("{val}", Number(newval) - 1));
+				target.before(new_rank);
 				refilled = true;
 			}
 			
 			if (refilled) {
 				enable_voting();
+				$(".ranking").each(function(index, element) {
+					$(element).find(".rankvalue").val(index + 1);
+					$(element).find(".candidatevalue").val(index + 1);
+				})
+			} else {
+				var newval = target.find('.rankvalue').val();
+				ui.item.find('input').val(newval);
 			}
 		}
 	}).disableSelection();
