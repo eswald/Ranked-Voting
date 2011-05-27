dd060132479839156a5e7387032a8bbfa890625c
Eric Wald <eswald@gmail.com>
2011-05-26 22:14:12 -0600

    Now adds a new rank after the target if the next rank is filled.
    Unfortunately, that currently leaves the following ranks with bad values.

--- static/voting.js
+++ static/voting.js
@@ -1,4 +1,6 @@
 var enable_voting = function() {
+	var new_rank = '<div class="ranking"><input type="number" class="rankvalue" value="{val}"></div>';
+	
 	$(".ranking").sortable({
 		connectWith: ".ranking",
 		receive: function(event, ui) {
@@ -6,7 +8,10 @@ var enable_voting = function() {
 			var newval = target.find('.rankvalue').val();
 			ui.item.find('input').val(newval);
 			if (!target.next().is(".ranking")) {
-				target.after('<div class="ranking"><input type="number" class="rankvalue" value="{val}"></div>'.replace("{val}", Number(newval) + 1));
+				target.after(new_rank.replace("{val}", Number(newval) + 1));
+				enable_voting();
+			} else if (target.next().has(".candidate").length) {
+				target.after(new_rank.replace("{val}", Number(newval) + 1));
 				enable_voting();
 			}
 		}
