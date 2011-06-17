fa54efdc7600e6040c1494139a5d7f28e7e04669
Eric Wald <eswald@gmail.com>
2011-06-17 15:15:35 -0600

    Splitting the matrix production into its own function.

--- research.py
+++ research.py
@@ -3,18 +3,18 @@ from itertools import combinations, permutations
 candidates = "ABCD"
 
 def well_sorted(ballot):
+    ballot = str.join("", ballot)
     for a, b in combinations(candidates[2:], 2):
         if (ballot.find(a) < ballot.find(b)) != (a < b):
             return False
     
     return True
 
-def main():
+def fully_ranked():
     pairs = sorted(str.join("", sorted(pair)) for pair in combinations(candidates, 2))
     head = pairs.pop(0)
     length = len(pairs)
     
-    count = 0
     for perm in permutations(pairs):
         for n in range(1 << length):
             tail = list(perm)
@@ -23,12 +23,16 @@ def main():
                     tail[-(bit+1)] = tail[-(bit+1)][::-1]
             
             tail.insert(0, head)
-            ballot = str.join(">", tail)
-            if not well_sorted(ballot):
+            if not well_sorted(tail):
                 continue
             
-            print ballot
-            count += 1
+            yield tail
+
+def main():
+    count = 0
+    for ballot in fully_ranked():
+        print str.join(">", ballot)
+        count += 1
     
     print count
 
