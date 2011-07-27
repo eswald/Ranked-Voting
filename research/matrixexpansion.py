946dd488ab4214e327c6b5e577e9000d6e7c8bc3
Eric Wald <eswald@gmail.com>
2011-07-27 09:57:09 -0600

    Switching to PuLP and GLPK for solutions.

--- research/matrixexpansion.py
+++ research/matrixexpansion.py
@@ -1,5 +1,9 @@
 from itertools import combinations, count, permutations
 
+# sudo apt-get install glpk python-glpk
+# pip install pulp
+from pulp import GLPK, LpVariable, LpProblem, LpMinimize, LpStatus, value as LpValue
+
 from voting.methods import methods
 
 class Sum(dict):
@@ -8,7 +12,7 @@ class Sum(dict):
         self.values = namespace
     
     def __call__(self):
-        return sum(self.values[name] * self.counts[name] for name in self.counts)
+        return sum(LpValue(self.values[name]) * self.counts[name] for name in self.counts)
     
     def __iter__(self):
         for name in self.counts:
@@ -19,9 +23,9 @@ class Sum(dict):
     
     def __sub__(self, other):
         result = self.__class__(self.values)
-        for name in self.counts:
-            count = self.counts[name] - other.counts.get(name, 0)
-            if count > 0:
+        for name in set(self.counts.keys() + other.counts.keys()):
+            count = self.counts.get(name, 0) - other.counts.get(name, 0)
+            if count != 0:
                 result.counts[name] = count
         return result
     
@@ -30,7 +34,15 @@ class Sum(dict):
     
     def __nonzero__(self):
         return any(self.counts.values())
-            
+    
+    def sum(self):
+        return sum(self.counts[name] * self.values[name] for name in self.counts)
+    
+    def __gt__(self, other):
+        return (self - other).sum() >= 1
+    
+    def __eq__(self, other):
+        return self.sum() == other.sum()
 
 class BallotFinder(object):
     r'''Finds a sample set of ballots for a given pairwise matrix.
@@ -41,7 +53,7 @@ class BallotFinder(object):
         self.constraints = []
         
         perms = [str.join("", perm) for perm in permutations(c.lower() for c in candidates)]
-        self.variables = dict.fromkeys(perms, 10)
+        self.variables = dict((name, LpVariable(name, 0, cat="Integer")) for name in perms)
         
         self.candidates = dict((c, Sum(self.variables, (perm for perm in perms if perm.startswith(c.lower())))) for c in candidates)
         
@@ -60,7 +72,7 @@ class BallotFinder(object):
         if varname.startswith("Borda-"):
             return self.bordas[varname[6:]]
         if varname in self.variables:
-            return Sum([varname])
+            return Sum(self.variables, [varname])
         if varname in self.candidates:
             return self.candidates[varname]
         if varname in self.pairs:
@@ -70,95 +82,19 @@ class BallotFinder(object):
     def constrainGreater(self, greater, lesser):
         bigger = self.check(greater)
         smaller = self.check(lesser)
-        bigger, smaller = bigger - smaller, smaller - bigger
-        if bigger or smaller:
-            print "%s > %s: %s > %s" % (greater, lesser, bigger, smaller)
-            self.constraints.append((bigger, smaller, True))
-        else:
-            # A given set of variables cannot be greater than itself.
-            raise ValueError("Unsatisfiable constraint")
+        print "%s > %s: %s > %s" % (greater, lesser, bigger, smaller)
+        self.problem += bigger > smaller
     
     def constrainEqual(self, left, right):
         first = self.check(left)
         second = self.check(right)
-        first, second = first - second, second - first
-        if first or second:
-            print "%s = %s: %s = %s" % (left, right, first, second)
-            self.constraints.append((first, second, False))
-    
-    def iterate(self, iteration):
-        # Errors: negative contributions, positive contributions.
-        # When sorted by value, variables too large should be at the head,
-        # variables too small at the tail.
-        errors = dict((key, [0, 0]) for key in self.variables)
-        constraints = []
-        total_errors = 0
-        for left, right, greater in self.constraints:
-            difference = left() - right()
-            if greater:
-                difference -= 1
-                if difference >= 0:
-                    left_bars = [0, difference]
-                    right_bars = [0, -difference]
-                    difference = 0
-                else:
-                    left_bars = [-difference, 0]
-                    right_bars = [difference, 0]
-            else:
-                left_bars = [-difference, 0]
-                right_bars = [difference, 0]
-            total_errors += abs(difference)
-            
-            for name in left:
-                errors[name][0] += left_bars[0]
-                errors[name][1] += left_bars[1]
-            for name in right:
-                errors[name][0] += right_bars[0]
-                errors[name][1] += right_bars[1]
-            
-            if difference:
-                low, high = (left, right) if difference < 0 else (right, left)
-                constraints.append((abs(difference), low, high))
-        
-        if not constraints:
-            return True
-        
-        constraints.sort()
-        fixer = constraints[-1]
-        lows = sorted(fixer[1], key=errors.get)
-        highs = sorted(fixer[2], key=errors.get)
-        
-        for item in highs:
-            if self.variables[item] and item not in self.backsies:
-                high = item
-                break
-        else:
-            high = highs[0]
-        
-        for item in reversed(lows):
-            if item not in self.backsies:
-                low = item
-                break
-        else:
-            low = lows[-1]
-        
-        self.backsies = [high, low]
-        
-        #for name in lows:
-        #    print "Low:", name, self.variables[name], errors[name], name in self.backsies, name is low
-        #for name in highs:
-        #    print "High:", name, self.variables[name], errors[name], name in self.backsies, name is high
-        print ("%d: %d; moving 1 from %s (%d: %d/%d) to %s (%d: %d/%d)" % (
-                iteration, total_errors,
-                high, self.variables[high], errors[high][0], errors[high][1],
-                low, self.variables[low], errors[low][0], errors[low][1],
-        ))
-        
-        self.variables[low] += 1
-        if self.variables[high] >= 1:
-            self.variables[high] -= 1
+        print "%s = %s: %s = %s" % (left, right, first, second)
+        self.problem += first == second
     
     def setup(self, statement):
+        self.problem = LpProblem(statement, LpMinimize)
+        self.problem += sum(self.variables.values())
+        
         for a, b in combinations(self.candidates, 2):
             ab = a + b
             ba = b + a
@@ -170,8 +106,8 @@ class BallotFinder(object):
                 self.constrainGreater(ab, ba)
             elif ba in statement:
                 self.constrainGreater(ba, ab)
-            else:
-                self.constrainEqual(ab, ba)
+            #else:
+            #    self.constrainEqual(ab, ba)
         
         prev = None
         for rank in statement.split(">"):
@@ -195,17 +131,13 @@ class BallotFinder(object):
                 self.constrainGreater("Borda-"+winner, "Borda-"+c)
     
     def solve(self):
-        self.backsies = []
-        solved = False
-        iteration = count(1)
-        while not solved:
-            solved = self.iterate(next(iteration))
-        
-        return self.variables
+        #print self.problem
+        status = self.problem.solve(GLPK(msg = 0))
+        print "Status:", LpStatus[status]
     
     def report(self):
         values = [
-            ((-self.variables[name], name) for name in self.variables),
+            ((-LpValue(self.variables[name]), name) for name in self.variables),
             ((-self.pairs[name](), name) for name in self.pairs),
             ((-self.candidates[name](), name) for name in self.candidates),
             ((-self.bordas[name](), "Borda-"+name) for name in self.bordas),
@@ -215,8 +147,8 @@ class BallotFinder(object):
             for value, name in sorted(valueset):
                 print "%s = %s" % (name, -value)
         
+        ballots = [(key.upper(), LpValue(self.variables[key])) for key in self.variables]
         for method in methods:
-            ballots = [(key.upper(), self.variables[key]) for key in self.variables]
             result = methods[method](ballots, set(self.candidates))
             rewritten = " > ".join("=".join(sorted(r)) for r in result)
             print "%-8s\t%s" % (method+":", rewritten)
