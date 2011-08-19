43f956ddcfe8a5640c7038a8894b1a3ba99bf5bc
Eric Wald <eswald@gmail.com>
2011-08-19 09:53:43 -0600

    Grouping Borda over Plurality, and removing invalid results.

--- research/runner.py
+++ research/runner.py
@@ -3,15 +3,16 @@ from permutations import with_equality
 
 def main(candidates = "ABCD"):
     solver = BallotFinder(candidates)
-    print ",".join(["Statement", "Plurality", "Borda", "Result"] + list(solver.headers()))
+    print ",".join(["Statement", "Borda", "Plurality", "Result"] + list(solver.headers()))
     for statement in with_equality(candidates):
-        for pc in candidates:
-            for bc in candidates:
+        for bc in candidates:
+            for pc in candidates:
                 solver.setup(statement)
                 solver.plurality(pc)
                 solver.borda(bc)
                 status = solver.solve()
-                print ",".join([statement, pc, bc, status] + list(map(str, solver.results())))
+                results = list(map(str, solver.results())) if status != "Infeasible" else []
+                print ",".join([statement, bc, pc, status] + results)
 
 if __name__ == "__main__":
     from sys import argv
